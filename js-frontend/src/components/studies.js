06c389d0d9dff583297ba9e1fc14ba6456b8238d
stroughk <katerynastrough@hotmail.com>
2020-01-18 18:34:27 -0600

    render json data and create a delete method

--- js-frontend/src/components/studies.js
+++ js-frontend/src/components/studies.js
@@ -1,27 +1,55 @@
 class Studies {
-    constructor() {
-        this.studies = []
-        this.adapter = new StudiesAdapter()
-        //this.bindEventListeners()
-        this.fetchAndLoadStudies()
-    }
+  constructor() {
+    this.studies = [];
+    this.adapter = new StudiesAdapter();
+    //this.bindEventListeners()
+    this.fetchAndLoadStudies();
 
-    fetchAndLoadStudies() {
-        this.adapter
-        .getStudies()
-        .then(studies => {
-            studies.forEach(study => this.studies.push(new Study(study)))
-            console.log(this.studies)
-        })
-        .then(() => {
-            this.render()
-        })
-    }
+    this.deleteStudy = this.deleteStudy.bind(this);
+    this.render = this.render.bind(this);
+  }
 
-    render() {
-        const studiesContainer = document.getElementById('studies-container')
-        studiesContainer.innerHTML = 'my studies here'
+  fetchAndLoadStudies() {
+    this.adapter
+      .getStudies()
+      .then(studies => {
+          /*
+        studies.forEach(study => {
+            this.studies.push(new Study(study))
+        });
+        */
+        this.studies = studies.map(study => new Study(study));
+      })
+      .then(() => {
+        this.render();
+      });
+  }
 
-    }
+  deleteStudy(id) {
+    console.log("got hereee", id);
+  }
+
+  render() {
+    const studyList = document.querySelector("#study-list");
+    studyList.innerHTML = '';
+    this.studies.forEach(study => {
+      const row = document.createElement("tr");
+      row.innerHTML = `
+                <td>${study.programming_language}</td>
+                <td>${study.topic}</td>
+                <td>${study.description}</td>`;
+      const deleteRow = document.createElement("td");
+      const deleteButton = document.createElement("button");
+      deleteButton.className = "btn btn-danger btn-sm delete";
+      deleteButton.innerHTML = "Delete";
+      deleteButton.onclick = () => {
+        this.adapter.deleteStudy(study.id).then(() => {
+          this.fetchAndLoadStudies();
+        });
+      };
+      deleteRow.appendChild(deleteButton);
+      row.appendChild(deleteRow);
+      studyList.appendChild(row);
+    });
+  }
 }
-
--- js-frontend/src/components/study.js
+++ js-frontend/src/components/study.js
@@ -1,5 +1,6 @@
 class Study {
     constructor(studyJSON) {
+        this.id = studyJSON.id
         this.programming_language = studyJSON.programming_language
         this.topic = studyJSON.topic
         this.description = studyJSON.description
